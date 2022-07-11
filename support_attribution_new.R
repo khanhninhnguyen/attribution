b774c8f043a975ac8cac9a0a39fe431ba6e274b0
khanhninhnguyen <khanhninhsp@gmail.com>
2022-07-11 19:00:50 +0100

    change the check cluster function

--- support_attribution_new.R
+++ support_attribution_new.R
@@ -1,21 +1,25 @@
-diff.var.1 <- function(name.test){
-  if(name.test == "gps.gps"){
-    varname = c("GPS", "GPS1")
+# old code
+check_cluster <- function(list.cluster, station, list.ind.cluster, breakpoint.c){
+  cluster = list.cluster[which(list.cluster$name == station),]
+  ind.sta = list.ind.cluster[which(list.ind.cluster$name == station),]
+  if( nrow(ind.sta) > 0){
+    cluster.ind = sapply(c(1:nrow(ind.sta)), function(x) which(cluster$end[ind.sta$deb[x]] < breakpoint.c & breakpoint.c < cluster$begin[ind.sta$fin[x]]), simplify = "array")
+    cluster.pre = which(cluster.ind==1)
+    if(length(cluster.pre) != 0){
+      cls.begin = cluster$end[ind.sta$deb[cluster.pre]]
+      cls.end = cluster$begin[ind.sta$fin[cluster.pre]]
+    }else {
+      cls.begin = max.d
+      cls.end = min.d
+    }
+  } else {
+    cls.begin = max.d
+    cls.end = min.d
   }
-  if(name.test == "gps.era"){
-    varname = c("GPS", "ERA")
-  }
-  if(name.test == "gps1.era"){
-    varname = c("GPS1", "ERA")
-  }
-  if(name.test == "gps.era1"){
-    varname = c("GPS", "ERA1")
-  }
-  if(name.test == "gps1.era1"){
-    varname = c("GPS1", "ERA")
-  }
-  if(name.test == "era.era"){
-    varname = c("ERA", "ERA1")
-  }
-  return(varname)
+  return(list(cls.begin = cls.begin, cls.end = cls.end))
 }
+
+# new code use the meta_compare file, read only the cluster 
+meeta.compare =  get(load(file = paste0(path_results,"validation/",nb_test.ref = 34,"-",criterion,"metacompa",screen.value="",".RData")))
+
+## continue!!!!!
