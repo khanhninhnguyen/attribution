817d2e6626f4e08caf297f5cc8c96910aaf5a5ca
khanhninhnguyen <khanhninhsp@gmail.com>
2022-11-02 22:58:19 +0000

    test vif principle and add hac

--- multicolinearity.R
+++ multicolinearity.R
@@ -8,7 +8,7 @@ library(attempt)
 library(nlme)
 
 source(paste0(path_code_att, "newUsed_functions.R"))
-win.thres = 10
+win.thres = 1
 dat = get(load( file = paste0(path_results,"attribution/data.all_", win.thres,"years_", nearby_ver,"screened.RData")))
 name.series <- "gps.era"
 one.year=365
@@ -145,3 +145,33 @@ for (k in list.ind) {
 }
 tot.res$VIF.selected[which(is.na(tot.res$VIF.selected)==TRUE)] <- -1
 
+
+# check the theoretical VIF 
+vif.HAC <- function(mod, vcov.matrix, ...) {
+  if (any(is.na(coef(mod)))) 
+    stop ("there are aliased coefficients in the model")
+  v <- vcov.matrix
+  assign <- attr(model.matrix(mod), "assign")
+  if (names(coefficients(mod)[1]) == "(Intercept)") {
+    v <- v[-1, -1]
+    assign <- assign[-1]
+  }
+  else warning("No intercept: vifs may not be sensible.")
+  terms <- labels(terms(mod))
+  n.terms <- length(terms)
+  if (n.terms < 2) stop("model contains fewer than 2 terms")
+  R <- cov2cor(v)
+  detR <- det(R)
+  result <- matrix(0, n.terms, 3)
+  rownames(result) <- terms
+  colnames(result) <- c("GVIF", "Df", "GVIF^(1/(2*Df))")
+  for (term in 1:n.terms) {
+    subs <- which(assign == term)
+    result[term, 1] <- det(as.matrix(R[subs, subs])) *
+      det(as.matrix(R[-subs, -subs])) / detR
+    result[term, 2] <- length(subs)
+  }
+  if (all(result[, 2] == 1)) result <- result[, 1]
+  else result[, 3] <- result[, 1]^(1/(2 * result[, 2]))
+  return(list(VIF = result, R = R))
+}
