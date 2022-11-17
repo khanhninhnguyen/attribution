b3e81436c037f02a67e1cc893597dedacb25b48c
khanhninhnguyen <khanhninhsp@gmail.com>
2022-11-17 19:00:41 +0000

    test

--- testN.R
+++ testN.R
@@ -1368,3 +1368,224 @@ glsControl <-
          optimMethod = optimMethod, natural = natural, sigma = sigma)
   }
 
+
+
+# variance matrix ---------------------------------------------------------
+
+Skip to content
+Product
+Solutions
+Open Source
+Pricing
+Search
+Sign in
+Sign up
+femiguez
+/
+  nlraa
+Public
+Code
+Issues
+3
+Pull requests
+Actions
+Projects
+Security
+Insights
+nlraa/R/var_cov.R
+@femiguez
+femiguez Added numerical derivatives to predict2_nls and expanded documentatio…
+…
+Latest commit 2409ac9 on Dec 13, 2021
+History
+1 contributor
+187 lines (164 sloc)  7.47 KB
+
+#' Variance Covariance matrix for (non)linear mixed models 
+#' 
+#' @title Variance Covariance matrix of for g(n)ls and (n)lme models
+#' @name var_cov
+#' @description Extracts the variance covariance matrix (residuals, random or all)
+#' @param object object which inherits class \code{\link[stats]{lm}}, \code{\link[nlme]{gls}} or \code{\link[nlme]{lme}}
+#' @param type \dQuote{residual} for the variance-covariance for the residuals, \dQuote{random}
+#' for the variance-covariance of the random effects or \dQuote{all} for the sum of both.
+#' @param aug whether to augment the matrix of the random effects to the dimensions of the data
+#' @param sparse whether to return a sparse matrix (default is FALSE)
+#' @param data optional passing of \sQuote{data}, probably needed when using this function inside other functions.
+#' @note See Chapter 5 of Pinheiro and Bates. This returns potentially a very large 
+#' matrix of N x N, where N is the number of rows in the data.frame. 
+#' The function \code{\link[nlme]{getVarCov}} only works well for  
+#' \code{\link[nlme]{lme}} objects. \cr
+#' The equivalence is more or less: \cr
+#' getVarCov type = \dQuote{random.effects} equivalent to var_cov type = \dQuote{random}. \cr
+#' getVarCov type = \dQuote{conditional} equivalent to var_cov type = \dQuote{residual}. \cr
+#' getVarCov type = \dQuote{marginal} equivalent to var_cov type = \dQuote{all}. \cr
+#' The difference is that getVarCov has an argument that specifies the individual 
+#' for which the matrix is being retrieved and var_cov returns the full matrix only.
+#' @return It returns a \code{\link[base]{matrix}} or a sparse matrix \code{\link[Matrix]{Matrix}}.
+#' @seealso \code{\link[nlme]{getVarCov}}
+#' @export
+#' @examples 
+#' \donttest{
+#' require(graphics)
+#' require(nlme)
+#' data(ChickWeight)
+#' ## First a linear model
+#' flm <- lm(weight ~ Time, data = ChickWeight)
+#' vlm <- var_cov(flm)
+#' ## First model with no modeling of the Variance-Covariance
+#' fit0 <- gls(weight ~ Time, data = ChickWeight)
+#' v0 <- var_cov(fit0)
+#' ## Only modeling the diagonal (weights)
+#' fit1 <- gls(weight ~ Time, data = ChickWeight, weights = varPower())
+#' v1 <- var_cov(fit1)
+#' ## Only the correlation structure is defined and there are no groups
+#' fit2 <- gls(weight ~ Time, data = ChickWeight, correlation = corAR1())
+#' v2 <- var_cov(fit2)
+#' ## The correlation structure is defined and there are groups present
+#' fit3 <- gls(weight ~ Time, data = ChickWeight, correlation = corCAR1(form = ~ Time | Chick))
+#' v3 <- var_cov(fit3)
+#' ## There are both weights and correlations
+#' fit4 <- gls(weight ~ Time, data = ChickWeight, 
+#'             weights = varPower(),
+#'             correlation = corCAR1(form = ~ Time | Chick))
+#' v4 <- var_cov(fit4)
+#' ## Tip: you can visualize these matrices using
+#' image(log(v4[,ncol(v4):1]))
+#' }
+
+var_cov <- function(object, type = c("residual","random","all", "conditional", "marginal"), 
+                    aug = FALSE, sparse = FALSE, data = NULL){
+  
+  type <- match.arg(type)
+  
+  if(type == "conditional") type <- "residual"
+  if(type == "marginal") type <- "all"
+  
+  if(type == "random" && inherits(object, c("lm","nls","gls")))
+    stop("The variance-covariance of the random effects is only available for \n
+          objects which inherit class 'lme' ")
+  
+  if(isTRUE(sparse)){
+    if(!requireNamespace("Matrix", quietly = TRUE)){
+      warning("Matrix package is required for this option")
+      return(NULL)
+    }
+  }
+  
+  if(inherits(object, c("lm","nls"))){
+    ans <- diag(nrow = length(fitted(object))) * sigma(object)^2  
+    if(sparse) ans <- Matrix::Matrix(ans, sparse = TRUE)
+  }
+  
+  if(inherits(object, c("gls","lme"))){
+    if(type == "residual"){
+      ans <- var_cov_lme_resid(object, sparse = sparse, data = data)  
+    }
+    if(type == "random"){
+      ans <- var_cov_lme_ranef(object, aug = aug, sparse = sparse, data = data)  
+    }
+    if(type == "all"){
+      ans <- var_cov_lme_resid(object, sparse = sparse, data = data) + var_cov_lme_ranef(object, aug = TRUE, sparse = sparse, data = data)  
+    }
+  }
+  
+  return(ans)
+}
+
+var_cov_lme_resid <- function(object, sparse = FALSE, data = data){
+  
+  if(!inherits(object, c("gls", "lme"))) 
+    stop("Only for objects which inherit the 'gls' or 'lme' class")
+  
+  if(inherits(object, "gls")){
+    sgms <- attr(residuals(object), "std")  
+  }else{
+    sgms <- attr(object[["residuals"]], "std")
+  }
+  
+  ## If there is no correlation structure this should work
+  if(is.null(object$modelStruct$corStruct)){
+    Lambda <- diag(sgms^2)     
+  }else{
+    if(is.null(object$groups)){
+      ## When groups are not present corMatrix returns a matrix of appropriate dimensions
+      Lambda <- t(sgms * corMatrix(object$modelStruct$corStruct)) * sgms
+    }else{
+      ## When groups are present corMatrix returns a list and it needs to be converted to
+      ## a block diagonal matrix. However, the order of the elements will not necessarily match
+      ## how they apprear in the dataset and this is a problem
+      corrMat <- corMatrix(object$modelStruct$corStruct)
+      
+      if(is.list(corrMat)){
+        ## If it is not a list it should return a matrix of appropriate dimensions
+        ## This extracts the group name
+        grp.nm <- deparse(nlme::getGroupsFormula(object$modelStruct$corStruct)[[2]])
+        ## I need to extract the groups in the original order in which they appear
+        ## in the dataset
+        ## Is unique guranteed to return the levels in the order in which they appear?
+        ## This is needed because of an evaulation issue when this function is nested
+        ## within other functions
+        if(is.null(data)){
+          gdat <- nlme::getData(object)
+        }else{
+          gdat <- data
+        } 
+        ogrpo <- unique(gdat[[grp.nm]])
+        ## This reorders the list in the order in which they appear in the dataset
+        corrMat <- corrMat[ogrpo]
+      }
+      
+      Lambda <- t(sgms * as.matrix(Matrix::bdiag(corrMat))) * sgms
+    }
+  }
+  
+  ## package Matrix is 'suggested'
+  if(sparse){
+    Lambda <- Matrix::Matrix(Lambda, sparse = TRUE)
+  }
+  
+  return(Lambda)
+}
+
+## This works for objects of class gls, lme and nlme (1 level)
+var_cov_lme_ranef <- function(object, aug = FALSE, sparse = FALSE, data = NULL){
+  
+  if(!inherits(object, c("gls", "lme"))) 
+    stop("Only for objects which inherit the 'gls' or 'lme' class")
+  
+  if(inherits(object, "nlme") && aug)
+    stop("Don't know how to augment nlme random effects.")
+  
+  ## Number of levels for the random effects
+  lreg <- length(names(object$modelStruct$reStruct))
+  
+  ## If there is only one level and we do not augment
+  if(lreg == 1L && aug == FALSE){
+    ## This should work for lme and nlme
+    ## But not for every object as reStruct[[1]] might not
+    ## be coercible to a matrix
+    ans <- as.matrix(object$modelStruct$reStruct[[1]]) * sigma(object)^2    
+  }else{
+    if(!inherits(object, "nlme")){
+      ## Does this work for 'lme' object with more than one group?
+      V <- mgcv::extract.lme.cov(object, data = data) ## This is V + Z %*% Vr %*% t(Z)
+      ## This is clearly potentially dangerous as it might return
+      ## negative values, need to test it
+      ans <- V - var_cov_lme_resid(object, data = data)      
+    }else{
+      ans <- vector("list", lreg)
+      names(ans) <- names(object$modelStruct$reStruct)
+      for(i in 1:lreg){
+        tm <- as.matrix(object$modelStruct$reStruct[[i]]) * sigma(object)^2 
+        if(sparse) tm <- Matrix::Matrix(tm, sparse = TRUE)
+        ans[[i]] <- tm
+      }
+    }
+  }
+  
+  if(sparse && !is.list(ans)) ans <- Matrix::Matrix(ans, sparse = TRUE)
+  
+  return(ans)
+}
+
--- test_OLSHAC_GLS.R
+++ test_OLSHAC_GLS.R
@@ -3,10 +3,10 @@ source(paste0(path_code_att,"simulate_time_series.R"))
 library(sandwich)
 # initial condition ---------------------------------------------------------
 nb.sim = 1000
-n = 2000
+n = 200
 sig.m = 0.6
 sig.v = 0.4
-T1 = n/6
+T1 = n/2
 a = cos(2*pi*(c(1:n)/T1) )
 var.t = (sig.m - sig.v*a)
 res = data.frame(matrix(NA, ncol = 6, nrow = nb.sim))
@@ -240,6 +240,64 @@ ggplot(dat.plot, aes(x = delta, y = value, col = variable))+
   geom_point() + theme_bw()+
   ylab("True positive rate") 
 
+
+# heteroskedastic + AR(1) -----------------------------------------------------------------------
+
+off.set = 0.3
+# var.all = seq(0, 0.5, 0.1)
+ar.val = seq( 0, 0.8, 0.2)
+param.test =ar.val
+Res.fin = data.frame(matrix(NA, ncol = 3, nrow = length(param.test)))
+trend = -0.01
+for (l in c(1:length(param.test))) {
+  # sig.v = param.test[l]
+  tot.res <- list()
+  coef.res <- list()
+  var.res <- list()
+  ar = param.test[l]
+  # var.t = (sig.m - sig.v*a)
+  # plot(var.t)
+  for (i in c(1:nb.sim)) {
+    set.seed(i)
+    
+    y = trend * t + simulate.general(N = n, arma.model = c(ar,0), burn.in = 1000, hetero = 1, sigma = sqrt(var.t),
+                         monthly.var = 0)
+    y[(n/2):n] <- y[(n/2):n] + off.set
+    Data.mod = data.frame(signal = y, jump = rep(c(0,1), each = n/2), var.t = var.t, t = t)
+    
+    # Test with vcov (HAC)
+    ols.fit = lm(signal~jump+t, data = Data.mod)
+    vcov.para=sandwich::kernHAC(ols.fit,prewhite = FALSE,kernel = "Quadratic Spectral", sandwich = TRUE)
+    fit.hac=lmtest::coeftest(ols.fit,df=(n-1),vcov.=vcov.para)[, ] %>% as.data.frame()
+    fit.ols=lmtest::coeftest(ols.fit,df=(n-1))[, ] %>% as.data.frame()
+    
+    gls.fit.true = gls(signal~jump+t, data = Data.mod,  correlation =  corAR1(form = ~t), weights = varFixed(value = ~var.t ))
+    fit.gls.true =lmtest::coeftest(gls.fit.true,df=(n-1))[, ] %>% as.data.frame()
+    
+    tot.res[[i]] = list(fit.hac =fit.hac, fit.ols = fit.ols, fit.gls.true = fit.gls.true)
+    coef.res[[i]] = list( ols = ols.fit$coefficients, gls = gls.fit.true$coefficients)
+    var.res[[i]] = list( ols = vcov(ols.fit), gls = gls.fit.true$varBeta, hac = vcov.para)
+    
+  }
+  # significance level
+  pval.ols <- unlist(sapply(c(1:nb.sim), function(x) tot.res[[x]]$fit.ols[2,4]))
+  pval.hac <- unlist(sapply(c(1:nb.sim), function(x) tot.res[[x]]$fit.hac[2,4]))
+  pval.gls.true <-  unlist(sapply(c(1:nb.sim), function(x) tot.res[[x]]$fit.gls.true[2,4]))
+  p.val = c(length(which(pval.ols>0.05)),
+            length(which(pval.gls.true>0.05)), length(which(pval.hac>0.05)))
+  Res.fin[l,] = p.val
+  # r[[l]] = length(which( pval.gls>0.05))
+}
+colnames(Res.fin) <- c("ols", "gls.t", "hac")
+res = (nb.sim - Res.fin)/nb.sim
+name.x = "rho"
+# res = Res.fin/nb.sim
+res[name.x] = param.test
+dat.plot =reshape2::melt(res, id = name.x)
+ggplot(dat.plot, aes(x = rho, y = value, col = variable))+
+  geom_point() + theme_bw()+
+  ylab("True Positive rate") 
+
 # test the variance estimation --------------------------------------------
 tot.res <- data.frame(matrix(NA, ncol = 200, nrow = nb.sim))
 for (i in c(1:nb.sim)) {
@@ -275,4 +333,55 @@ theme_bw() +
 
 
 
+
+
+
+# inspect the impact of multicollinearity ---------------------------------
+
+nb.sim = 1000
+off.set = 0.3
+trend = 0
+# var.all = seq(0, 0.5, 0.1)
+ar.val = seq( 0, 0.8, 0.2)
+param.test =ar.val
+coef.all = data.frame(matrix(NA, ncol = 2, nrow = nb.sim))
+var.all = data.frame(matrix(NA, ncol = 2, nrow = nb.sim))
+for (i in c(1:nb.sim)) {
+  set.seed(i)
+  y = rnorm(n, 0, sd = sqrt(0.6))
+  # y = trend*t+ simulate.general(N = n, arma.model = c(0,0), burn.in = 1000, hetero = 0, sigma = sqrt(sig.m),
+  #                      monthly.var = 0)
+  # y[(n/2):n] <- y[(n/2):n] + off.set
+  Data.mod = data.frame(signal = y, jump = rep(c(0,1), each = n/2), var.t = rep(sig.m, n), t = t)
+  
+  # Test with vcov (HAC)
+  # gls.fit.true = gls(signal~jump, data = Data.mod,  correlation =  NULL, weights = NULL)
+  # gls.fit.true.t = gls(signal~jump+t, data = Data.mod,  correlation =  NULL, weights = NULL)
+  ols.fit = lm(signal~jump, data = Data.mod)
+  # vcov.para=sandwich::kernHAC(ols.fit,prewhite = FALSE,kernel = "Quadratic Spectral", sandwich = TRUE)
+
+  ols.fit.t = lm(signal~jump+t, data = Data.mod)
+  # vcov.para.t=sandwich::kernHAC(ols.fit.t,prewhite = FALSE,kernel = "Quadratic Spectral", sandwich = TRUE)
+
+  # coef.all[i,] = c(gls.fit.true$coefficients[2],gls.fit.true.t$coefficients[2] )
+  # var.all[i,] = c(gls.fit.true$varBeta[2,2],gls.fit.true.t$varBeta[2,2])
+  coef.all[i,] = c(ols.fit$coefficients[2],ols.fit.t$coefficients[2] )
+  var.all[i,] = c(vcov(ols.fit)[2,2],vcov(ols.fit.t )[2,2])
+  # var.all[i,] = c(vcov.para[2,2],vcov.para.t[2,2])
+}
+# histo of the variance
+start.c = round(min(var.all), digits = 2) - 0.01
+end.c = round(max(var.all), digits = 2) +0.01
+hist(var.all$X1, col=rgb(0,0,1,0.2), seq(start.c, end.c, 0.0005), xlim = c(0, end.c), main = "Histogram of var(jump)", xlab = "")
+hist(var.all$X2, col=rgb(1,0,0,0.2), seq(start.c, end.c, 0.0005), add=TRUE, xlab = "")
+legend('topright', c('jump', 'jump+trend'),
+       fill=c(rgb(0,0,1,0.2), rgb(1,0,0,0.2)))
+
+# histo of the estimates
+start.c = round(min(coef.all), digits = 1) - 0.1
+end.c = round(max(coef.all), digits = 1) +0.1
+hist(coef.all$X1, col=rgb(0,0,1,0.2), breaks = seq(start.c, end.c, 0.01), xlim = c(-0.5, 1), main = "Histogram of jump estimates", xlab = "")
+hist(coef.all$X2, col=rgb(1,0,0,0.2), breaks = seq(start.c, end.c, 0.01), add=TRUE, xlab = "")
+legend('topright', c('jump', 'jump+trend'),
+       fill=c(rgb(0,0,1,0.2), rgb(1,0,0,0.2)))
 
