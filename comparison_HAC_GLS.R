bfda37482e3d2fede47175d38fe0422dadfd79a4
khanhninhnguyen <khanhninhsp@gmail.com>
2022-12-05 23:07:54 +0000

    test

--- comparison_HAC_GLS.R
+++ comparison_HAC_GLS.R
@@ -3,10 +3,10 @@ source(paste0(path_code_att,"simulate_time_series.R"))
 source(paste0(path_code_att,"newUsed_functions.R"))
 
 # input: what do you want to test. Ex: TPR of test when data is AR(1) with different rho------------------
-off.set = 0.3
+off.set = 0
 heteroscedast = 1
-autocor = 0
-x.axis = "sig.v"
+autocor = 1
+x.axis = "rho"
 
 y.axis = ifelse(off.set !=0, "TPR", "FPR")
 nb.sim = 1000
@@ -49,12 +49,12 @@ mod.sim <- function(heteroscedastic, autocorr, var.inno, list.param.sig, list.pa
   return(list(hetero = hetero, burn.in = burn.in, sigma.t = sigma.t, ar = ar))
 }
 gen.test = mod.sim(heteroscedastic = heteroscedast, autocorr = autocor, var.inno = 0.4, list.param.ar = list.param.ar, 
-                   list.param.sig = list.param.sig, x.axis = x.axis, individual = 6, n = n, T1 = n/2)
+                   list.param.sig = list.param.sig, x.axis = x.axis, individual = 3, n = n, T1 = n/2)
 
 
 
 res.var = data.frame(matrix(NA, ncol = 2, nrow = nb.sim))
-Res.fin = data.frame(matrix(NA, ncol = 4, nrow = length(gen.test$ar)))
+Res.fin = data.frame(matrix(NA, ncol = 3, nrow = length(gen.test$ar)))
 
 hetero = gen.test$hetero
 burn.in = gen.test$burn.in
@@ -64,7 +64,7 @@ for (l in c(1:length(gen.test$ar))) {
   coef.res <- list()
   var.res <- list()
   ar = gen.test$ar[l]
-  sigma.sim = gen.test$sigma.t[,l]
+  sigma.sim = gen.test$sigma.t[l]
   for (i in c(1:nb.sim)) {
     set.seed(i)
     y = simulate.general(N = n, arma.model = c(ar,0), burn.in = burn.in, hetero = hetero, sigma = sqrt(sigma.sim),
@@ -75,16 +75,16 @@ for (l in c(1:length(gen.test$ar))) {
     # Test with vcov (HAC)
     ols.fit = lm(mod.expression, data = Data.mod)
     if(ar ==0 & hetero == 0){
-      vcov.para=sandwich::kernHAC(ols.fit,prewhite = FALSE, kernel = "Quadratic Spectral",adjust = TRUE, sandwich = TRUE)
+      vcov.para=sandwich::kernHAC(ols.fit, prewhite = TRUE, kernel = "Quadratic Spectral",adjust = TRUE, sandwich = TRUE)
       gls.fit <- eval(parse(text=paste0("gls(",mod.expression,",data=Data.mod,correlation = NULL, na.action=na.omit,weights=NULL",")")))
     }else if(ar ==0 & hetero != 0){
-      vcov.para=sandwich::kernHAC(ols.fit,prewhite = FALSE, kernel = "Quadratic Spectral", adjust = TRUE, sandwich = TRUE)
+      vcov.para=sandwich::kernHAC(ols.fit, prewhite = TRUE, kernel = "Quadratic Spectral", adjust = TRUE, sandwich = TRUE)
       gls.fit <- eval(parse(text=paste0("gls(",mod.expression,",data=Data.mod,correlation = NULL, na.action=na.omit,weights=varFixed(value = ~weight1)",")")))
     }else if(ar != 0 & hetero != 0){
-      vcov.para=sandwich::kernHAC(ols.fit,prewhite = FALSE, approx = c("AR(1)"), kernel = "Quadratic Spectral", adjust = TRUE, sandwich = TRUE)
+      vcov.para=sandwich::kernHAC(ols.fit, prewhite = TRUE, approx = c("AR(1)"), kernel = "Quadratic Spectral", adjust = TRUE, sandwich = TRUE)
       gls.fit <- eval(parse(text=paste0("gls(",mod.expression,",data=Data.mod,correlation =  corAR1(ar, form = ~ 1), na.action=na.omit,weights=varFixed(value = ~weight1)",")")))
     }else if(ar != 0 & hetero == 0){
-      vcov.para=sandwich::kernHAC(ols.fit,prewhite = FALSE, approx = c("AR(1)"), kernel = "Quadratic Spectral", adjust = TRUE, sandwich = TRUE)
+      vcov.para=sandwich::kernHAC(ols.fit, prewhite = TRUE, approx = c("AR(1)"), kernel = "Quadratic Spectral", adjust = TRUE, sandwich = TRUE)
       gls.fit <- eval(parse(text=paste0("gls(",mod.expression,",data=Data.mod,correlation =  corAR1(ar, form = ~ 1), na.action=na.omit,weights=NULL",")")))
     }
     
@@ -95,27 +95,32 @@ for (l in c(1:length(gen.test$ar))) {
     fit.gls =lmtest::coeftest(gls.fit,df=(n-2-trend.reg))[, ] %>% as.data.frame()
     
     #GLS with true covariance matrix
-    gls.fit.true = gls.true(var.t = Data.mod$weight1, phi = ar, theta = 0, design.matrix = Data.mod, trend = trend.reg)
+    # gls.fit.true = gls.true(var.t = Data.mod$weight1, phi = ar, theta = 0, design.matrix = Data.mod, trend = trend.reg)
     
-    tot.res[[i]] = list(fit.hac =fit.hac, fit.ols = fit.ols, fit.gls.true = gls.fit.true$fit.gls, fit.gls = fit.gls)
-    coef.res[[i]] = list( ols = ols.fit$coefficients, gls = gls.fit$coefficients, gls.t = gls.fit.true$Coefficients)
-    var.res[[i]] = list( ols = vcov(ols.fit), gls = gls.fit$varBeta, hac = vcov.para, gls.t = gls.fit.true$vcov)
+    tot.res[[i]] = list(fit.hac =fit.hac, fit.ols = fit.ols,  fit.gls = fit.gls)
+    coef.res[[i]] = list( ols = ols.fit$coefficients, gls = gls.fit$coefficients)
+    var.res[[i]] = list( ols = vcov(ols.fit), gls = gls.fit$varBeta, hac = vcov.para)
     
+    # tot.res[[i]] = list(fit.hac =fit.hac, fit.ols = fit.ols, fit.gls.true = gls.fit.true$fit.gls, fit.gls = fit.gls)
+    # coef.res[[i]] = list( ols = ols.fit$coefficients, gls = gls.fit$coefficients, gls.t = gls.fit.true$Coefficients)
+    # var.res[[i]] = list( ols = vcov(ols.fit), gls = gls.fit$varBeta, hac = vcov.para, gls.t = gls.fit.true$vcov)
+    # 
   }
   # significance level
   pval.ols <- unlist(sapply(c(1:nb.sim), function(x) tot.res[[x]]$fit.ols[2,4]))
   pval.hac <- unlist(sapply(c(1:nb.sim), function(x) tot.res[[x]]$fit.hac[2,4]))
-  pval.gls.true <-  unlist(sapply(c(1:nb.sim), function(x) tot.res[[x]]$fit.gls.true[2,4]))
+  # pval.gls.true <-  unlist(sapply(c(1:nb.sim), function(x) tot.res[[x]]$fit.gls.true[2,4]))
   pval.gls <-  unlist(sapply(c(1:nb.sim), function(x) tot.res[[x]]$fit.gls[2,4]))
   
   p.val = c(length(which(pval.ols>0.05)),
-            length(which(pval.gls.true>0.05)), 
+            # length(which(pval.gls.true>0.05)), 
             length(which(pval.gls>0.05)), 
             length(which(pval.hac>0.05)))
   Res.fin[l,] = p.val
 }
 
-colnames(Res.fin) <- c("ols", "gls.t","gls", "hac")
+colnames(Res.fin) <- c("OLS","FGLS", "OLS-HAC")
+save(Res.fin, file = paste0(path_results, "attribution/h.", heteroscedast, "a.", autocor, x.axis, y.axis, "trend.reg", trend.reg,"1.RData"))
 if(off.set == 0){
   res = (nb.sim- Res.fin)/nb.sim
 }else{
@@ -125,23 +130,30 @@ name.x = x.axis
 if(x.axis == "rho"){
   param.test = list.param.ar
 }else{
-  param.test = list.param.sig
+  param.test = list.param.sig*100/0.4
 }
 res[name.x] = param.test
 dat.plot =reshape2::melt(res, id = name.x)
 face1 = "bold"
+x.axis1 = expression(phi)
+# linetype1 = "dashed"
 jpeg(paste0(path_results,"attribution/h.", heteroscedast, "a.", autocor, x.axis, y.axis, "trend.reg", trend.reg,"1.jpg" ),
      width = 2600, height = 1800,res = 300)
 p2 <- eval(parse(
   text=paste0("ggplot(dat.plot, aes(x =", x.axis, ",y = value, col = variable))+
-  geom_point(size=3) + theme_bw() +
-  ylab(y.axis) +
-  theme(axis.text = element_text(size = 18),legend.text=element_text(size=15),
-      axis.title = element_text(size=18,face=face1))")))
+  geom_point(size=3) + geom_line() +theme_bw() +
+  ylab(y.axis) + 
+  xlab(x.axis1) +
+  scale_x_continuous(breaks=seq(0, 1, 0.15), limits =c(0,0.9))+
+  scale_y_continuous(breaks=seq(0, 1, 0.15), limits =c(0,1))+
+  theme(axis.text = element_text(size = 25),legend.text=element_text(size=15),legend.title=element_blank(),
+      axis.title = element_text(size=25,face=face1))")))
 print(p2)
 dev.off()
-
-
+# scale_x_continuous(breaks=seq(25,87.5,12.5))+
+# scale_y_continuous(breaks=seq(0, 0.7, 0.15), limits =c(0,0.7))+
+# scale_y_continuous(breaks=seq(0, 1, 0.15), limits =c(0,1))+
+  
 a = cos(2*pi*(c(1:n)/T1) )
 v = rep(NA,6)
 for (l in c(1:6)) {
@@ -163,7 +175,7 @@ trend.reg = 0
 t = c(1:n)-n/2
 type.hc = "HC3"
 kernel1 = "Quadratic Spectral"
-approx1 = c("ARMA(1,1)")
+approx1 = c("AR(1)")
 mod.expression = ifelse(trend.reg == 1, "signal~jump+Xt", "signal~jump")
 
 name.sim <- function(name.model, var.m, var.t, ar0){
@@ -294,7 +306,5 @@ write.table( res.glsnmle,
 write.table( res.glstrue, 
              file = paste0(path_results, "attribution/sim.GLStrue", trend.reg, off.set,".txt"),
              sep = "\t", quote = FALSE)
-
-
 
 
