4d33cb4e3964222506b74d5aecebfd7ed63b9c6e
khanhninhnguyen <khanhninhsp@gmail.com>
2023-01-21 22:53:56 +0000

    test

--- check_performance_FGLS.R
+++ check_performance_FGLS.R
@@ -3,32 +3,36 @@ list.param.ar = seq(0,0.9,0.15)
 list.param.sig = seq(0.1, 0.45, 0.05)
 
 hetero = 1
-autocor = 0
-y.axis = "TPR"
+autocor = 1
+y.axis = "FPR"
 jump = ifelse(y.axis=="FPR", 0, 0.3)
 noise.name = "ar1"
-k = 1
+k = 7
 x.axis = "rho"
+# true.sim.sd = sqrt(0.5 - list.param.sig[k]*cos(2*pi*(c(1:730)/365) -pi))
+# ar = 0.3
 
-if(hetero==0){
-  if(autocor==1){
-    ar = list.param.ar[k]
-  }
-  true.sim.sd = sqrt(0.5)
-}else{
-  if(autocor==1){
-    ar = 0.3
-  }else{
-    if(x.axis == "rho"){
-      true.sim.sd = sqrt(0.5 - list.param.sig[4]*cos(2*pi*(c(1:730)/365) -pi))
-    }else{}
-    ar =0
-    x.axis = "sig"
-    noise.name = "white"
-    true.sim.sd = sqrt(0.5 - list.param.sig[k]*cos(2*pi*(c(1:730)/365) -pi))
-    # true.sim.sd = sqrt(0.5 - list.param.sig[4]*cos(2*pi*(c(1:730)/365) -pi))
-  }
-}
+true.sim.sd = sqrt(0.5 - list.param.sig[4]*cos(2*pi*(c(1:730)/365) -pi))
+ar = list.param.ar[k]
+# if(hetero==0){
+#   if(autocor==1){
+#     ar = list.param.ar[k]
+#   }
+#   true.sim.sd = sqrt(0.5)
+# }else{
+#   if(autocor==1){
+#     ar = 0.3
+#   }else{
+#     if(x.axis == "rho"){
+#       true.sim.sd = sqrt(0.5 - list.param.sig[4]*cos(2*pi*(c(1:730)/365) -pi))
+#     }else{}
+#     ar =0
+#     x.axis = "sig"
+#     noise.name = "white"
+#     true.sim.sd = sqrt(0.5 - list.param.sig[k]*cos(2*pi*(c(1:730)/365) -pi))
+#     # 
+#   }
+# }
 Res = get(load(file = paste0(path_results,"attribution/N1000/",hetero,"auto",autocor, x.axis, y.axis,noise.name,"R.Data")))
 
 true.sd.beta = sqrt(Res$total[[k]][[1]]$gls$vcov[1,1])
@@ -93,5 +97,5 @@ abline(v=mean.est, col ="blue")
 dev.off()
 
 table(fgls.t>1.96)
-
-
+# check value 
+fgls.sd
