a9bd69189001c0bb825e05be1cb5c1be7a7f8ea8
khanhninhnguyen <khanhninhsp@gmail.com>
2023-03-20 00:08:56 +0000

    add test for length

--- performance_autoarima_wrongmodel.R
+++ performance_autoarima_wrongmodel.R
@@ -8,7 +8,6 @@ length.wind = 60
 ## only wrong model: MA(1)
 arma.coef = c(0.6, -0.3)
 model.est = c(1,0,1)
-N = 1000
 Tot.res.AR = list()
 set.seed(1)
 for (i in c(1:nb.sim)) {
@@ -20,6 +19,28 @@ for (i in c(1:nb.sim)) {
   Tot.res.AR[[i]] =  fgls.fit
 }
 save(Tot.res.AR, file = paste0(path_results, "attribution0/truemodel.ARMA1.RData"))
+## for different length
+arma.coef = c(0.3, 0)
+model.est = c(1,0,0)
+N = 1000
+Tot.res.AR = list()
+Tot.res = list()
+length.list = c(200, 600, 1000, 1400)
+for (j in c(1:length(length.list))) {
+  N = length.list[j]
+  set.seed(1)
+  for (i in c(1:nb.sim)) {
+    y = simulate.general1(N = N, arma.model = arma.coef, burn.in = 1000, hetero = 0, sigma = 1)
+    df = data.frame(y = y, date = seq(as.Date("2014-01-13"), as.Date("2018-05-27"), by="days")[1:N])
+    Data.mod = construct.design(data.df = df, name.series = "y", break.ind = N/2, one.year = N/2)
+    Data.mod =  Data.mod[,c(1,10,11)]
+    fgls.fit = FGLS1(design.m = Data.mod, tol=0.01, day.list = df$date, noise.model = model.est, length.wind)
+    Tot.res.AR[[i]] =  fgls.fit
+  }
+  Tot.res[[j]] = Tot.res.AR
+}
+
+save(Tot.res, file = paste0(path_results, "attribution0/truemodel.ARMA1.length.RData"))
 # Wrong model -------------------------------------------------------------
 ## only wrong model: a) AR(phi = 0.3) but identified as MA(1)---
 arma.coef = c(0.3,0)
@@ -37,7 +58,7 @@ for (i in c(1:nb.sim)) {
 }
 save(Tot.res.AR, file = paste0(path_results, "attribution0/truemodel.AR1.RData"))
 ## only wrong model: a) MA(phi = 0.2) but identified as AR(1)---
-arma.coef = c(0,0.2)
+arma.coef = c(0,0.3)
 model.est = c(1,0,0)
 N = 1000
 Tot.res.MA = list()
@@ -50,7 +71,7 @@ for (i in c(1:nb.sim)) {
   fgls.fit = FGLS1(design.m = Data.mod, tol=0.01, day.list = df$date, noise.model = model.est, length.wind)
   Tot.res.MA[[i]] =  fgls.fit
 }
-save(Tot.res.MA, file = paste0(path_results, "attribution0/wrongmodel.MA1.RData"))
+save(Tot.res.MA, file = paste0(path_results, "attribution0/wrongmodel.MA1a.RData"))
 
 ## only wrong model: a) MA(phi = 0.2) but identified as AR(1)---
 arma.coef = c(0.6,-0.3)
