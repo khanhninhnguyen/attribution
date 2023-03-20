1381dafac82db483b80a6f7b753c977aea9a84d5
khanhninhnguyen <khanhninhsp@gmail.com>
2023-03-20 16:50:08 +0000

    test

--- performance_autoarima_wrongmodel.R
+++ performance_autoarima_wrongmodel.R
@@ -5,11 +5,12 @@ nb.sim = 1000
 length.wind = 60
 
 # Start with true model --------------------------------------------------
-## only wrong model: MA(1)
+## only wrong model: MA(1)----
 arma.coef = c(0.6, -0.3)
-model.est = c(1,0,1)
+model.est = c(1,0,0)
 Tot.res.AR = list()
 set.seed(1)
+N=600
 for (i in c(1:nb.sim)) {
   y = simulate.general1(N = N, arma.model = arma.coef, burn.in = 1000, hetero = 0, sigma = 1)
   df = data.frame(y = y, date = seq(as.Date("2014-01-13"), as.Date("2018-05-27"), by="days")[1:N])
@@ -17,35 +18,36 @@ for (i in c(1:nb.sim)) {
   Data.mod =  Data.mod[,c(1,10,11)]
   fgls.fit = FGLS1(design.m = Data.mod, tol=0.01, day.list = df$date, noise.model = model.est, length.wind)
   Tot.res.AR[[i]] =  fgls.fit
+  print(i)
 }
-save(Tot.res.AR, file = paste0(path_results, "attribution0/truemodel.ARMA1.RData"))
-## for different length
-arma.coef = c(0.3, 0)
-model.est = c(1,0,0)
-N = 1000
-Tot.res.AR = list()
-Tot.res = list()
-length.list = c(200, 600, 1000, 1400)
-for (j in c(1:length(length.list))) {
-  N = length.list[j]
-  set.seed(1)
-  for (i in c(1:nb.sim)) {
-    y = simulate.general1(N = N, arma.model = arma.coef, burn.in = 1000, hetero = 0, sigma = 1)
-    df = data.frame(y = y, date = seq(as.Date("2014-01-13"), as.Date("2018-05-27"), by="days")[1:N])
-    Data.mod = construct.design(data.df = df, name.series = "y", break.ind = N/2, one.year = N/2)
-    Data.mod =  Data.mod[,c(1,10,11)]
-    fgls.fit = FGLS1(design.m = Data.mod, tol=0.01, day.list = df$date, noise.model = model.est, length.wind)
-    Tot.res.AR[[i]] =  fgls.fit
-  }
-  Tot.res[[j]] = Tot.res.AR
-}
-
-save(Tot.res, file = paste0(path_results, "attribution0/truemodel.ARMA1.length.RData"))
+save(Tot.res.AR, file = paste0(path_results, "attribution0/truemodel.MA1", N, ".RData"))
+## for different length----
+# arma.coef = c(0.3, 0)
+# model.est = c(0,0,1)
+# N = 1000
+# Tot.res.AR = list()
+# Tot.res = list()
+# length.list = c(200, 600, 1000, 1400)
+# for (j in c(1:length(length.list))) {
+#   N = length.list[j]
+#   set.seed(1)
+#   for (i in c(1:nb.sim)) {
+#     y = simulate.general1(N = N, arma.model = arma.coef, burn.in = 1000, hetero = 0, sigma = 1)
+#     df = data.frame(y = y, date = seq(as.Date("2014-01-13"), as.Date("2018-05-27"), by="days")[1:N])
+#     Data.mod = construct.design(data.df = df, name.series = "y", break.ind = N/2, one.year = N/2)
+#     Data.mod =  Data.mod[,c(1,10,11)]
+#     fgls.fit = FGLS1(design.m = Data.mod, tol=0.01, day.list = df$date, noise.model = model.est, length.wind)
+#     Tot.res.AR[[i]] =  fgls.fit
+#   }
+#   Tot.res[[j]] = Tot.res.AR
+# }
+# 
+# save(Tot.res, file = paste0(path_results, "attribution0/truemodel.AR1.length.RData"))
 # Wrong model -------------------------------------------------------------
 ## only wrong model: a) AR(phi = 0.3) but identified as MA(1)---
 arma.coef = c(0.3,0)
 model.est = c(1,0,0)
-N = 1000
+N = 200
 Tot.res.AR = list()
 set.seed(1)
 for (i in c(1:nb.sim)) {
@@ -56,11 +58,11 @@ for (i in c(1:nb.sim)) {
   fgls.fit = FGLS1(design.m = Data.mod, tol=0.01, day.list = df$date, noise.model = model.est, length.wind)
   Tot.res.AR[[i]] =  fgls.fit
 }
-save(Tot.res.AR, file = paste0(path_results, "attribution0/truemodel.AR1.RData"))
+save(Tot.res.AR, file = paste0(path_results, "attribution0/truemodel.AR1", N, ".RData"))
 ## only wrong model: a) MA(phi = 0.2) but identified as AR(1)---
 arma.coef = c(0,0.3)
 model.est = c(1,0,0)
-N = 1000
+N = 200
 Tot.res.MA = list()
 set.seed(1)
 for (i in c(1:nb.sim)) {
@@ -71,12 +73,12 @@ for (i in c(1:nb.sim)) {
   fgls.fit = FGLS1(design.m = Data.mod, tol=0.01, day.list = df$date, noise.model = model.est, length.wind)
   Tot.res.MA[[i]] =  fgls.fit
 }
-save(Tot.res.MA, file = paste0(path_results, "attribution0/wrongmodel.MA1a.RData"))
+save(Tot.res.MA, file = paste0(path_results, "attribution0/wrongmodel.MA1", N, ".RData"))
 
 ## only wrong model: a) MA(phi = 0.2) but identified as AR(1)---
 arma.coef = c(0.6,-0.3)
 model.est = c(1,0,0)
-N = 1000
+N = 200
 Tot.res.MA = list()
 set.seed(1)
 for (i in c(1:nb.sim)) {
@@ -87,13 +89,14 @@ for (i in c(1:nb.sim)) {
   fgls.fit = FGLS1(design.m = Data.mod, tol=0.01, day.list = df$date, noise.model = model.est, length.wind)
   Tot.res.MA[[i]] =  fgls.fit
 }
-save(Tot.res.MA, file = paste0(path_results, "attribution0/wrongmodel.ARMA1.RData"))
+save(Tot.res.MA, file = paste0(path_results, "attribution0/wrongmodel.ARMA", N, ".RData"))
 
 
 # plot results ------------------------------------------------------------
-true.ar = get(load(file = paste0(path_results, "attribution0/truemodel.MA1.RData")))
-wrong.ar = get(load(file = paste0(path_results, "attribution0/wrongmodel.MA1.RData")))
-true.model = "MA"
+## wrong model---
+true.ar = get(load(file = paste0(path_results,  "attribution0/truemodel.MA1", N, ".RData")))
+wrong.ar = get(load(file = paste0(path_results, "attribution0/wrongmodel.ARMA1.RData")))
+true.model = "ARMA"
 t.true.ar = sapply(c(1:nb.sim), function(x) true.ar[[x]]$t.table$`t value`[1] )
 t.wrong.ar = sapply(c(1:nb.sim), function(x) wrong.ar[[x]]$t.table$`t value`[1] )
 
@@ -144,5 +147,28 @@ p3 = ggplot(data = d, aes(x = value, col = variable))+
   ylab("CDF")+
   xlab("Std. Err")
 ggsave(filename = paste0(path_results, "attribution0/beta_",true.model,".jpg"), plot = p3)
+## length ---
+true.ar = get(load(file = paste0(path_results, "attribution0/truemodel.ARMA1.length.RData")))
+true.model = "AR"
+t.true.ar = as.data.frame(sapply(c(1:4), function(x) sapply(c(1:nb.sim), function(y) true.ar[[x]][[y]]$t.table$`t value`[1] ))) 
+colnames(t.true.ar) = as.factor(length.list)
+sapply(c(1:4), function(x) table(abs(t.true.ar[,x])>1.96))
+d = t.true.ar %>% reshape2::melt()
+p1 = ggplot(data = d, aes(x = value, col = variable))+
+  theme_bw()+
+  stat_ecdf()+
+  ylab("CDF")+
+  xlab("t-value")
+ggsave(filename = paste0(path_results, "attribution0/t_val_n_",true.model,".jpg"), plot = p1)
 
-
+t.true.ar = as.data.frame(sapply(c(1:4), function(x) sapply(c(1:nb.sim), function(y) true.ar[[x]][[y]]$t.table$`Std. Error`[1]))) 
+colnames(t.true.ar) = as.factor(length.list)
+sapply(c(1:4), function(x) table(abs(t.true.ar[,x])>1.96))
+d = t.true.ar %>% reshape2::melt()
+p2 = ggplot(data = d, aes(x = value, col = variable))+
+  theme_bw()+
+  stat_ecdf()+
+  scale_x_continuous(limits = c(0.0, 0.3), breaks = seq(0.0, 0.3,0.05))+
+  ylab("CDF")+
+  xlab("Std. Err")
+ggsave(filename = paste0(path_results, "attribution0/Std_err_n_",true.model,".jpg"), plot = p2)
